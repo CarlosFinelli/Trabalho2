7568fa869aafce0bf7ada2b46fc39f581b571ad9
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 11:06:31 -0300

    Conclusão questão 4

---
+++ Questao4/Questao4/Questao4/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao4
+{
+    class Funcionario
+    {
+        public int num, horas;
+        public double valorHora, salario;
+
+        public double Salario()
+        {
+            salario = valorHora * horas;
+            return Math.Round(salario, 2);
+        }
+    }
+}
---
+++ Questao4/Questao4/Questao4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Insira o número do funcionário: ");
+            funcionario.num = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o número de horas trabalhadas: ");
+            funcionario.horas = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor hora do funcionário: ");
+            funcionario.valorHora = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Number = {funcionario.num}");
+            Console.WriteLine($"Salary = {funcionario.Salario()}");
+        }
+    }
+}
