0332a6a80668005bf540b32abea360cc065bfa7b
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 13:55:36 -0300

    Conclusão questão 9

---
+++ Questao9/Questao9/Questao9/Numero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao9
+{
+    class Numero
+    {
+        public int num1, num2;
+
+        public String VerificaMultiplo()
+        {
+            if (num1 % num2 == 0 || num2 % num1 == 0)
+            {
+                return "São Multiplos";
+            }
+            return "Não são Multiplos";
+        }
+    }
+}
---
+++ Questao9/Questao9/Questao9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Questao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero numero = new Numero();
+            Console.Write("Insira um número: ");
+            numero.num1 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira outro número: ");
+            numero.num2 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(numero.VerificaMultiplo());
+            Console.ReadKey();
+        }
+    }
+}
