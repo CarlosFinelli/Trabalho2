844c1a20a2708a2fb9650ba1312442bb52582c7f
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 11:53:59 -0300

    Conclusão questão 6

---
+++ Questao6/Questao6/Questao6/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao6
+{
+    class Calculadora
+    {
+        public double A, B, C;
+        private double Pi = 3.14159;
+
+        public double AreaTriangulo()
+        {
+            double area = (A * C) / 2;
+            return Math.Round(area, 3);
+        }
+
+        public double AreaCirculo()
+        {
+            double area = Pi * (Math.Pow(C, 2));
+            return Math.Round(area, 3);
+        }
+
+        public double AreaTrapezio()
+        {
+            double area = ((A + B) / 2) * C;
+            return Math.Round(area, 3);
+        }
+
+        public double AreaQuadrado()
+        {
+            double area = Math.Pow(B, 2);
+            return Math.Round(area, 3);
+        }
+        public double AreaRetangulo()
+        {
+            double area = A * B;
+            return Math.Round(area, 3);
+        }
+    }
+}
---
+++ Questao6/Questao6/Questao6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Questao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.Write("Insira o valor de A: ");
+            calculadora.A = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de B: ");
+            calculadora.B = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de C: ");
+            calculadora.C = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Triangulo: {calculadora.AreaTriangulo()}");
+            Console.WriteLine($"Circulo: {calculadora.AreaCirculo()}");
+            Console.WriteLine($"Trapezio: {calculadora.AreaTrapezio()}");
+            Console.WriteLine($"Quadrado: {calculadora.AreaQuadrado()}");
+            Console.WriteLine($"Retangulo: {calculadora.AreaRetangulo()}");
+            Console.ReadKey();
+        }
+    }
+}
