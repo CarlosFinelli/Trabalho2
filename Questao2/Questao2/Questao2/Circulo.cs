efb60ff8e356b0eb7d82898854bc65b113103693
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 10:29:26 -0300

    Conclusão questão 2

---
+++ Questao2/Questao2/Questao2/Circulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao2
+{
+    class Circulo
+    {
+        public double r, area;
+        public double Pi = 3.14159;
+
+        public double Area()
+        {
+            area = Pi * Math.Pow(r, 2);
+            return Math.Round(area, 4);
+        }
+    }
+}
---
+++ Questao2/Questao2/Questao2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo();
+            Console.Write("Insira o valor do Raio: ");
+            circulo.r = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"A = {circulo.Area()}");
+        }
+    }
+}
