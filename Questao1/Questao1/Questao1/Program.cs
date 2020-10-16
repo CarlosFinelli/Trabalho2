bc5b334d97480905fd84c4a1823d15cdb7b881e1
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 10:17:55 -0300

    Conclusão questão 1

---
+++ Questao1/Questao1/Questao1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma soma = new Soma();
+            Console.Write("Insira o valor de A: ");
+            soma.A = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de B: ");
+            soma.B = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            soma.soma();
+            Console.WriteLine($"Soma = {soma.resultado}");
+        }
+    }
+}
---
+++ Questao1/Questao1/Questao1/Soma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao1
+{
+    class Soma
+    {
+        public double A, B, resultado;
+
+        public void soma()
+        {
+            resultado = A + B;
+        }
+    }
+}
