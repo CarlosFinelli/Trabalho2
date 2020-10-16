ab6cefa4c1dc41269e09c317292c2cb47c6a4563
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 11:00:33 -0300

    Conclusão questao 3

---
+++ Questao3/Questao3/Questao3/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao3
+{
+    class Calculadora
+    {
+        public double A, B, C, D;
+        public double diferenca, produto1, produto2;
+
+        public void Multiplicacao()
+        {
+            produto1 = A * B;
+            produto2 = C * D;
+        }
+        public double Diferenca()
+        {
+            diferenca = produto1 - produto2;
+            return diferenca;
+        }
+    }
+}
---
+++ Questao3/Questao3/Questao3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.Write("Insira o valor de A: ");
+            calculadora.A = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de B: ");
+            calculadora.B = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de C: ");
+            calculadora.C = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o valor de D: ");
+            calculadora.D = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            calculadora.Multiplicacao();
+            Console.WriteLine($"Diferença: {calculadora.Diferenca()}");
+
+        }
+    }
+}
