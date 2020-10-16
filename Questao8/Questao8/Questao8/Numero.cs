20fe159bdeccfebf4b39184c8140581acf68181d
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 13:49:46 -0300

    Conclusão questão 8

---
+++ Questao8/Questao8/Questao8/Numero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao8
+{
+    class Numero
+    {
+        public int num;
+
+        public String VerificaPar()
+        {
+            if (num % 2 == 0)
+            {
+                return "PAR";
+            }
+            return "ÍMPAR";
+        }
+    }
+}
---
+++ Questao8/Questao8/Questao8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero numero = new Numero();
+            Console.Write("Insira um número: ");
+            numero.num = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(numero.VerificaPar());
+            Console.ReadKey();
+        }
+    }
+}
