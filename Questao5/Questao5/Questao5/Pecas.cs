176e3d0a0e1c97dcbc83e04cf3d81ed892ff5255
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 11:19:07 -0300

    Conclusão questão 5

---
+++ Questao5/Questao5/Questao5/Pecas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao5
+{
+    class Pecas
+    {
+        public int cod1, cod2, num1, num2;
+        public double preco1, preco2;
+        public double valorTotal;
+
+        public double Valor()
+        {
+            valorTotal = (num1 * preco1) + (num2 * preco2);
+            return Math.Round(valorTotal, 2);
+        }
+    }
+}
---
+++ Questao5/Questao5/Questao5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pecas pecas = new Pecas();
+            Console.Write("Insira o código da peça 1: ");
+            pecas.cod1 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o número de peças que irá comprar: ");
+            pecas.num1 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o preço unitário da peça: ");
+            pecas.preco1 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o código da peça 2: ");
+            pecas.cod2 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o número de peças que irá comprar: ");
+            pecas.num2 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o preço unitário da peça: ");
+            pecas.preco2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Valor total a pagar: R${pecas.Valor()}");
+            Console.ReadKey();
+        }
+    }
+}
