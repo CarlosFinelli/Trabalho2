df746c2ef70c03dc40f73bf813c22e9bb29515f3
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 16:19:16 -0300

    Conclusão questão 10

---
+++ Questao10/Questao10/Questao10/Jogos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao10
+{
+    class Jogos
+    {
+        public int hora1, hora2;
+
+        public int Tempo()
+        {
+            int resultado1, resultado2;
+            int retorno = 0;
+            if (hora1 > hora2)
+            {
+                resultado1 = 24 - hora1;
+                resultado2 = 0 + hora2;
+                retorno = resultado1 + resultado2;
+            } else if (hora2 > hora1)
+            {
+                retorno = hora2 - hora1;
+            } else if (hora1 == hora2)
+            {
+                retorno = 24;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Questao10/Questao10/Questao10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Questao10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogos jogos = new Jogos();
+            Console.Write("Insira o horário do jogo 1: ");
+            jogos.hora1 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Insira o horário do jogo 2: ");
+            jogos.hora2 = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"O jogo durou {jogos.Tempo()} horas");
+        }
+    }
+}
