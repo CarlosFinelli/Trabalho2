3badc9f761b69f3c86232a1bbb8e01080f5d7fc1
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-16 13:45:38 -0300

    Conclusão questão 7

---
+++ Questao7/Questao7/Questao7/Numeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao7
+{
+    class Numeros
+    {
+        public double num;
+
+        public String VerificaNumero()
+        {
+            if (num > 0)
+            {
+                return "NÃO NEGATIVO";
+            } else
+            {
+                return "NEGATIVO";
+            }
+        }
+    }
+}
---
+++ Questao7/Questao7/Questao7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Questao7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros numeros = new Numeros();
+            Console.Write("Insira um número: ");
+            numeros.num = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(numeros.VerificaNumero());
+        }
+    }
+}
