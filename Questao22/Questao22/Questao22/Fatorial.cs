b8b9ac9e4a7b81dc0115cd0003b99c5859b612e0
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 13:24:55 -0300

    Conclusão questão 22

---
+++ Questao22/Questao22/Questao22/Fatorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao22
+{
+    class Fatorial
+    {
+        public int N, result = 1;
+        public int calculo() {
+            for (var i = N; i >= 1; i--)
+            {
+                result *= i;
+            }
+            return result;
+         }
+    }
+}
---
+++ Questao22/Questao22/Questao22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Questao22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fatorial fatorial = new Fatorial();
+            Console.Write("Insira o número que deseja o fatorial: ");
+            fatorial.N = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(fatorial.calculo());
+
+        }
+    }
+}
