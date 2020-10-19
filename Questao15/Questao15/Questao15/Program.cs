c14085816ba02f48c8fecb94c679899acd1db25f
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 09:45:56 -0300

    Conclusão questão 15

---
+++ Questao15/Questao15/Questao15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Questao15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 0;
+            while (senha != 2002)
+            {
+                Console.WriteLine("senha Inválida");
+                Console.Write("Insira a senha: ");
+                senha = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadKey();
+        }
+    }
+}
