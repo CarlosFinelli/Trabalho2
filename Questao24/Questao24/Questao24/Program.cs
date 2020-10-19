6241e0433d519f7f27ceb708c7e0c2fc8ea4f247
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 13:41:49 -0300

    Conclusão questão 24

---
+++ Questao24/Questao24/Questao24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Questao24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Digite um número: ");
+            N = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            for (var i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+        }
+    }
+}
