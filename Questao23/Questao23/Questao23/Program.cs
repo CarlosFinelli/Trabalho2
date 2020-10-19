d0fc54afc0d1cca7e44e7f0223cfb595a24845e6
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 13:38:49 -0300

    Conclusão questão 23

---
+++ Questao23/Questao23/Questao23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Questao23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Insira um número: ");
+            num = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            for (var i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
