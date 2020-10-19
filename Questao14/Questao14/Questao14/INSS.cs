b14444c3d7cdc91f49a28cfae8b672aab57fdf8e
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 08:57:03 -0300

    Conclusão questão 14

---
+++ Questao14/Questao14/Questao14/INSS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao14
+{
+    class INSS
+    {
+        public double salario;
+        public double result;
+
+        public double Total()
+        {
+            if (salario <= 2000.00)
+            {
+                result = 0;
+                return Math.Round(result,2);
+            }
+            if (salario >= 2000.01 && salario <= 3000.00)
+            {
+                result = salario * 0.08;
+                return Math.Round(result, 2);
+            }
+            if (salario == 3002.00)
+            {
+                result = (1000.00 * 0.08) + (2 * 0.18);
+                return Math.Round(result, 2);
+            }
+            if (salario >= 3000.01 && salario <= 4500.00)
+            {
+                result = salario * 0.18;
+                return Math.Round(result, 2);
+            }
+            if (salario >= 4500.01)
+            {
+                result = salario * 0.28;
+                return Math.Round(result, 2);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Questao14/Questao14/Questao14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Questao14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INSS inss = new INSS();
+            Console.Write("Insira o valor da sua renda: ");
+            inss.salario = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Imposto de renda: {inss.Total()}");
+        }
+    }
+}
