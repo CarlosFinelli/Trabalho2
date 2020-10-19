f9b460d26a3c5b3d99d614c86c241d5730c126df
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 10:02:36 -0300

    Conclusão questão 16

---
+++ Questao16/Questao16/Questao16/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Questao16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double y = 1;
+            while (x != 0 && y != 0)
+            {
+                Console.Write("Insira o valor de X: ");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+                Console.Write("Insira o valor de Y: ");
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("Quarto");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
