ed3d7be7bf2b43d4de9d7250229c84365adaaff2
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 10:59:37 -0300

    Conclusão questão 19

---
+++ Questao19/Questao19/Questao19/Numeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao19
+{
+    class Numeros
+    {
+        public int num, A = 0, B = 0, N;
+
+        public void Calculo()
+        {
+            if (num >= 10 && num <= 20)
+            {
+                A++;
+            } else
+            {
+                B++;
+            }
+
+        }
+    }
+}
---
+++ Questao19/Questao19/Questao19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Questao19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros numero = new Numeros();
+            Console.Write("Insira o número de números que deseja inserir: ");
+            numero.N = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            for (int i = 0; i < numero.N; i++)
+            {
+                Console.WriteLine("Insira um número: ");
+                numero.num = Convert.ToInt16(Console.ReadLine());
+                Console.Clear();
+                numero.Calculo();
+            }
+            Console.WriteLine($"In: {numero.A}");
+            Console.WriteLine($"Out: {numero.B}");
+        }
+    }
+}
