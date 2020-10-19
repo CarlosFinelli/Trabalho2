7e9e9f8f82b6e00dd060bb24ae9af53ee307525b
Olimpiadas <carlosmfinellI@gmail.com>
2020-10-19 10:46:42 -0300

    Conclusão questão 18

---
+++ Questao18/Questao18/Questao18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questao18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.Write("Insira um valor: ");
+            N = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            for (var i = 0; i <= N; i++ )
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
